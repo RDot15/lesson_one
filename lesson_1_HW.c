280f07fb1aed5146e1768b4239d918b17adace89
Dmitry Barinov <rdot1@yandex.ru>
2024-03-15 22:10:15 +0700

    ready

---
+++ lesson_1_HW.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+// Task_1 
+
+//     printf("Hello world");
+//     printf("\n\n"); 
+
+// // Task_2
+
+    // printf ("let's\n");
+    // printf ("   go\n");
+    // printf ("       to walk");
+
+    // printf("\n\n"); 
+
+// Task_3
+
+char * string_1[] = {"    *", "   ***", "  *****", " *******", NULL};
+char * string_2[]= {"  HH HH  " , "  ZZZZZ", NULL};
+
+for (int i=0;i<6;i++){
+   printf("%s \n",string_1[i]);
+}
+
+
+for (int j=0;j<6;j++){
+	printf("%s \n", string_2[j]);
+}
+
+
+//Task_4
+
+	
+	// printf ("   __     __ \n" ); 
+	
+	// printf ("  /  \\" "___/  \\ \n" );
+	
+		
+	// printf ("  \\  /   \\  / \n" ); 
+	// printf ("   __     __ \n" ); 
+
+	
+	// printf ("  /  \\" "___/  \\ \n" );
+	
+	
+	// printf ("  \\__/   \\__/ \n" ); 
+	
+	
+	
+	
+
+
+
+
+
+
+    return 0;
+}
+
+
+
